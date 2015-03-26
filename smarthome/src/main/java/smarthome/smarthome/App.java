f400e69f777d2a1b40bf48c171d1a8b45868cbdf
=afliu <afliu.zlg@gmail.com>
2015-03-26 14:47:08 +0800

    first commit

---
+++ smarthome/src/main/java/smarthome/smarthome/App.java
@@ -0,0 +1,13 @@
+package smarthome.smarthome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
